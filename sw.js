7688bda80f2b94dc33871b0a02e2315bb326c960
Manu <mpanizzo@octopus.com.ar>
2019-01-16 15:57:17 -0300

    cambios en cache

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('js/sw-utils.js');
 
 const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -51,6 +51,11 @@ self.addEventListener('activate', e => {
         keys.forEach(key => {
 
             if (key !== STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key);
+            }
+
+
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
                 return caches.delete(key);
             }
 
